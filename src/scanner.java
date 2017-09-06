ce7b52a53ed51edae4fc2231e0fca3d205c92874
Chase <esmeraldastan@users.noreply.github.com>
2017-09-06 12:41:45 -0700

    Scanner

---
+++ src/scanner.java
@@ -0,0 +1,14 @@
+//import
+import java.util.Scanner;
+
+public class scanner{
+public static void main(String args[]){
+	Scanner scanner = new Scanner(System.in);
+	
+	//ask for name 
+	System.out.println("Please enter your name?");
+	String name=scanner.next();
+	System.out.println("What is your age?");
+	int age=scanner.nextInt();
+ }
+}
