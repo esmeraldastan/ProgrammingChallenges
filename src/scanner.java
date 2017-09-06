1c8fb80024482250982a087e6c048a4b9cfed224
Chase <esmeraldastan@users.noreply.github.com>
2017-09-06 12:56:30 -0700

    9/06/17

--- src/scanner.java
+++ src/scanner.java
@@ -8,7 +8,11 @@ public static void main(String args[]){
 	//ask for name 
 	System.out.println("Please enter your name?");
 	String name=scanner.next();
+	System.out.println("is");
 	System.out.println("What is your age?");
 	int age=scanner.nextInt();
+	System.out.println("What is your favorite TV program?");
+	String tv=scanner.next();
+	
  }
 }
