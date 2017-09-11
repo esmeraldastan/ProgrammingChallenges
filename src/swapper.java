4d7b353aa7dbe4c753b4430489b82feb545f6abf
Chase <esmeraldastan@users.noreply.github.com>
2017-09-11 12:51:18 -0700

    save/9/11

--- src/swapper.java
+++ src/swapper.java
@@ -4,6 +4,7 @@ public class swapper {
 public static void main(String args[]){
 	Scanner swapper = new Scanner(System.in);
 
-	System.out.printIn("What is your first name and surname " );
+	System.out.println("What is your first name and surname " );
+	String name=swapper.next();
 }
 }
